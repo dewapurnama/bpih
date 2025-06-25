1c4a4088cc289eb5ce7714fcf70f059df2cd06f1
dewapurnama <75442383+dewapurnama@users.noreply.github.com>
2025-06-25 10:31:46 +0700

    Update bpih.py

--- bpih.py
+++ bpih.py
@@ -6,6 +6,7 @@ st.set_page_config(layout="wide")
 
 tab0, tab1, tab2, tab3 = st.tabs(["Dashboard", "Regional Analysis", "AI Predictions", "Data Documentation"])
 
+tab0.subheader(":bar_chart: Dashboard Realisasi SBN DJPPR")
 tab0.markdown(
     "<h1 style='font-size:25px;'>📊 Data Historis dan Proyeksi BPIH</h1>",
     unsafe_allow_html=True
