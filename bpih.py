1918386318ed1f42bf6bbce2e8ecb633da8f94a7
dewapurnama <75442383+dewapurnama@users.noreply.github.com>
2025-06-25 10:31:09 +0700

    Update bpih.py

--- bpih.py
+++ bpih.py
@@ -7,7 +7,7 @@ st.set_page_config(layout="wide")
 tab0, tab1, tab2, tab3 = st.tabs(["Dashboard", "Regional Analysis", "AI Predictions", "Data Documentation"])
 
 tab0.markdown(
-    "<h1 style='font-size:50px; color:#4CAF50;'>📊 Dashboard BPIH</h1>",
+    "<h1 style='font-size:25px;'>📊 Data Historis dan Proyeksi BPIH</h1>",
     unsafe_allow_html=True
 )
 
