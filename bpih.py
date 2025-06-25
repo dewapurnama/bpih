be97d1b62faf3dd1986c0862c2cfcd35d8e65107
dewapurnama <75442383+dewapurnama@users.noreply.github.com>
2025-06-25 10:22:45 +0700

    Update bpih.py

--- bpih.py
+++ bpih.py
@@ -4,23 +4,5 @@ import plotly.graph_objs as go
 
 st.set_page_config(layout="wide")
 
-# ----- Horizontal Navigation Menu -----
-selected = option_menu(
-    menu_title=None,  # Hide the title
-    options=["Dashboard", "Regional Analysis", "AI Predictions", "Data Documentation"],
-    icons=["bar-chart", "globe", "robot", "file-earmark-text"],
-    menu_icon="cast",
-    orientation="horizontal",
-    default_index=0,
-    styles={
-        "container": {"padding": "0!important", "background-color": "#ffffff"},
-        "icon": {"color": "#8e44ad", "font-size": "18px"},
-        "nav-link": {
-            "font-size": "16px",
-            "text-align": "center",
-            "margin": "0px",
-            "--hover-color": "#eee",
-        },
-        "nav-link-selected": {"background-color": "#e8e8e8"},
-    },
-)
+tab0,tab1, tab2 = st.tabs(["Dashboard", "Regional Analysis", "AI Predictions", "Data Documentation"])
+
